e22ea186bc5984b523a289e773115402e9b32bad
kayleekim409 <81453056+kayleekim409@users.noreply.github.com>
2021-10-20 21:11:25 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
+mustashX=0;
+mustashY=0;
+
 function preload() {
-
+mustash = loadImage('mustash.jpg');
 }
+
 function setup() {
-    canvas = createCanvas(300, 300);
-    canvas.center();
-}
-function draw() {
+canvas = createCanvas(300, 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
 
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
 }
-function saveFilter() {
-    
+
+function modelLoaded() {
+console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+if(results.length > 0)
+{
+console.log(results);
+noseX = results[0].pose.nose.x;
+noseY = results[0].pose.nose.y;
+}
+}
+
+function draw() {
+image(video, 0, 0, 300, 300);
+image(mustash, mustashX, mustashY, 30, 30);
+}
+
+function take_snapshot(){
+save('myFilterImage.png');
 }
